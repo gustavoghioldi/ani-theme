d365f1076eea7f635d7bbd6eefd3fe4d8b4a8f3d
gustavoghioldi <gustavoghioldi@gmail.com>
2016-01-10 12:28:33 -0300

    cambios menores en pruduct

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -13,8 +13,16 @@ angular.module('yapp')
     $http.get("http://esocialcommerce.devteam.com.ar/producto")
         .then(function(response) {$scope.products = response.data;});
 
-    $scope.publicar = function(id){
+    $scope.publish = function(id){
         $state.go("products/publishProducts", {productId:id});
+    }
+
+    $scope.edit = function(id){
+    	alert("quiere editar: "+id)
+    }
+
+    $scope.delete = function(id){
+    	alert("quiere borrar: "+id);
     }
 
 });
