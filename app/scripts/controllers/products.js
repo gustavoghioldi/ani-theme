0ed997ce077c5ad69dd0d5f671eba18380577723
gustavoghioldi <gustavoghioldi@gmail.com>
2016-01-11 21:32:37 -0300

    simple-table

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -9,7 +9,17 @@
  */
 
  angular.module('yapp')
- .controller('ProductsCtrl' ,function($scope,$http, $state, ProductsService ) {
+ .controller('ProductsCtrl' ,function($scope,$filter, $http, $state, ProductsService ) {
+    //////////////////////////////////
+    $scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    $scope.predicates = ['firstName', 'lastName', 'birthDate', 'balance', 'email'];
+    $scope.selectedPredicate = $scope.predicates[0];
+    ///////////////////////////////////
     getProducts();
 
     $scope.publish = function(id){
