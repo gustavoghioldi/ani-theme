c7d5319b18dc13d7afe8bac91f2c8783fb846867
gustavoghioldi <gustavoghioldi@gmail.com>
2016-01-24 19:50:42 -0300

    ahora puedo editar

--- app/scripts/controllers/editProducts.js
+++ app/scripts/controllers/editProducts.js
@@ -11,9 +11,27 @@ angular.module('yapp')
   .controller('EditProductsCtrl', function($scope, $http, $location, ProductsService) {	
 
   	$scope.product = ProductsService.data;
-
+  	$scope.editProduct = {};
   	$scope.submit = function(){
   		console.log($scope.product);
+  		$scope.editProduct.nombre = $scope.product.nombre;
+  		$scope.editProduct.codigo = $scope.product.codigo;
+  		$scope.editProduct.descripcion = $scope.product.descripcion;
+  		$scope.editProduct.precio = $scope.product.precio;
+  		$scope.editProduct.stock = $scope.product.stock;
+  		
+  		$http({
+  		  method: 'PUT',
+    	  url: "http://esocialcommerce.devteam.com.ar/producto/"+$scope.product.id,
+    	  data: ($scope.editProduct),
+    	  headers: {'Content-Type': 'application/json'},
+
+		}).success(function(res){
+				
+			alert("Su articulo a sido modificado con exito!!!");
+      $location.url("products");
+		});
+  			 
   	}
 
 });
